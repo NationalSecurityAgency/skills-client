47453c645aefa97ce4d571135f3c51783fc02edf
Matthew Kirkley <mtkirkl@evoforge.org>
2019-06-04 14:37:17 -0400

    #142 reporter passing back responses to directive user

--- src/directive/SkillsReporterDirective.js
+++ src/directive/SkillsReporterDirective.js
@@ -1,13 +1,20 @@
 import SkillsReporter from '@skills/skills-client-reporter';
 
-const eventListener = skillId => () => {
-  SkillsReporter.reportSkill(skillId);
+const RESPONSE_EVENT = 'skills-report-response';
+
+const eventListener = (el, skillId) => () => {
+  SkillsReporter.reportSkill(skillId)
+    .then((result) => {
+      const event = new CustomEvent(RESPONSE_EVENT, { detail: result });
+      el.dispatchEvent(event);
+    })
+    .catch((error) => {
+      const event = new CustomEvent(RESPONSE_EVENT, { detail: error });
+      el.dispatchEvent(event);
+    });
 };
 
-const eventContext = {
-  name: null,
-  handler: null,
-};
+const eventCache = new WeakMap();
 
 export default {
   name: 'skills',
@@ -15,11 +22,16 @@ export default {
     SkillsReporter.initialize(serviceUrl, projectId, authenticationUrl);
   },
   inserted(el, binding) {
-    eventContext.name = binding.arg ? binding.arg : 'click';
-    eventContext.handler = eventListener(binding.value);
+    const eventContext = {
+      name: binding.arg ? binding.arg : 'click',
+      handler: eventListener(el, binding.value),
+    };
     el.addEventListener(eventContext.name, eventContext.handler);
+    eventCache.set(el, eventContext);
   },
   unbind(el) {
+    const eventContext = eventCache.get(el);
     el.removeEventListener(eventContext.name, eventContext.handler);
+    eventCache.delete(el);
   },
 };
