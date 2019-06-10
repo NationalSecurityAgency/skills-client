073b749e25962c5f5249fb94561ddd4e9a374d42
EVOFORGE\dimay <dimay@evoforge.org>
2019-06-10 14:47:34 -0400

    vuejs example - WIP

--- skills-example-service/src/main/java/skills/example/Controller.java
+++ skills-example-service/src/main/java/skills/example/Controller.java
@@ -1,46 +1,46 @@
 package skills.example;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.HttpClient;
+import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class Controller {
 
-//    @CrossOrigin(origins = "http://localhost:8091")
-//    @GetMapping("/users/{user}/token")
-//    public OAuth2Response getUserAuthToken(@PathVariable String user) {
-//        String serviceTokenUrl = "http://localhost:8080/oauth/token";
-//        String clientId = "movies";
-//        String clientSecret = "1wWFWoP43fL6n0Mx81u6FkaW567iDIDF";
-//
-//        RestTemplate oAuthRestTemplate = new RestTemplate();
-//        oAuthRestTemplate.setInterceptors(Arrays.asList(new BasicAuthenticationInterceptor(clientId, clientSecret)));
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-//
-//        MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
-//        body.add("grant_type", "client_credentials");
-//        body.add("proxy_user", user);
-//
-//        ResponseEntity<OAuth2Response> responseEntity = oAuthRestTemplate.postForEntity(serviceTokenUrl, new HttpEntity<>(body, headers), OAuth2Response.class);
-//
-//        return responseEntity.getBody();
-//    }
+    @Value("${skills.service.url}")
+    String serviceUrl;
 
-    @CrossOrigin(origins = "http://localhost:8091")
+    @Value("${skills.service.projectId}")
+    String projectId;
+
+    @Value("${skills.service.username}")
+    String username;
+
+    @Value("${skills.service.password}")
+    String password;
+
+    @CrossOrigin()
     @GetMapping("/users/{user}/token")
     public String getUserAuthToken1(@PathVariable String user) {
-        String serviceTokenUrl = "http://localhost:8080/oauth/token";
-        String clientId = "movies";
-        String clientSecret = "1wWFWoP43fL6n0Mx81u6FkaW567iDIDF";
+        String clientId = projectId;
+        String serviceTokenUrl = serviceUrl + "/oauth/token";
+        String clientSecret = getSecret();
 
         RestTemplate oAuthRestTemplate = new RestTemplate();
         oAuthRestTemplate.setInterceptors(Arrays.asList(new BasicAuthenticationInterceptor(clientId, clientSecret)));
@@ -56,24 +56,51 @@ public class Controller {
         return responseEntity.getBody();
     }
 
-//    static class OAuth2Response {
-//        @JsonProperty("access_token")
-//        String accessToken;
-//        @JsonProperty("token_type")
-//        String tokenType;
-//        @JsonProperty("expires_in")
-//        Long expiresIn;
-//        String scope;
-//        @JsonProperty("proxy_user")
-//        String proxyUser;
-//        String jti;
-//
-//        public String getAccessToken() {
-//            return accessToken;
-//        }
-//
-//        public void setAccessToken(String accessToken) {
-//            this.accessToken = accessToken;
-//        }
-//    }
+    @GetMapping("/skills")
+    List<String> getAvailableSkillIds() throws Exception{
+        String skillsUrl = serviceUrl + "/admin/projects/" + projectId + "/skills";
+        RestTemplate restTemplate = getTemplateWithAuth();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(skillsUrl, String.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode topLevelTree = objectMapper.readTree(responseEntity.getBody());
+        List<String> res = new ArrayList<>();
+        for (JsonNode skillNode : topLevelTree) {
+            String skillId = skillNode.get("skillId").asText();
+            res.add(skillId);
+        }
+        System.out.println(responseEntity.getBody());
+        return res;
+    }
+
+    private String getSecret() {
+        String secretUrl = serviceUrl + "/admin/projects/" + projectId + "/clientSecret";
+        RestTemplate restTemplate = getTemplateWithAuth();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(secretUrl, String.class);
+        return responseEntity.getBody();
+    }
+
+    private RestTemplate getTemplateWithAuth(){
+        RestTemplate restTemplate = new RestTemplate();
+        // must configure HttpComponentsClientHttpRequestFactory as SpringTemplate does
+        // not by default keeps track of session
+        restTemplate.setRequestFactory(getHttpRequestFactory());
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add("username", username);
+        params.add("password", password);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(params, headers);
+        ResponseEntity<String> response = restTemplate.postForEntity(serviceUrl + "/performLogin", request, String.class);
+        assert response.getStatusCode() == HttpStatus.OK;
+        return restTemplate;
+    }
+
+    private ClientHttpRequestFactory getHttpRequestFactory() {
+        HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+        HttpClient httpClient = HttpClientBuilder.create().setSSLHostnameVerifier(new AllowAllHostnameVerifier()).build();
+        clientHttpRequestFactory.setHttpClient(httpClient);
+        return clientHttpRequestFactory;
+    }
+
 }
