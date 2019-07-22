05e6bac4caa4e577406c30b588f797bf7ff22a05
Ryan Mayo <rmmayo@evoforge.org>
2019-07-22 11:00:27 -0400

    #155 - added VueEntryPointFilter for skills-example-service

---
+++ skills-example-service/src/main/java/skills/example/filters/VueEntryPointFilterUtils.java
@@ -0,0 +1,28 @@
+package skills.example.filters;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+
+@Component
+public class VueEntryPointFilterUtils {
+
+    @Value("#{\"${skills.vue.entry.backend.resources:/api,/static,/favicon.ico,/skills.ico}\".split(\",\")}")
+    private java.util.List<java.lang.String> backendResources;
+
+    public boolean isFrontendResource(java.lang.String pathInfo) {
+        return !isBackendResource(pathInfo);
+    }
+
+    public boolean isBackendResource(final java.lang.String pathInfo) {
+        return asBoolean(backendResources) ? backendResources.stream().filter(ignoreUrl -> pathInfo.startsWith(ignoreUrl)).findFirst().isPresent() : false;
+    }
+
+    public static boolean asBoolean(Collection collection) {
+        if (null == collection) {
+            return false;
+        }
+        return !collection.isEmpty();
+    }
+}
