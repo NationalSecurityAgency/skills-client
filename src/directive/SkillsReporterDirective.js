b1c35b939dd7c6f9b9a395edd4f9c0bb20ef116b
Matthew Kirkley <mtkirkl@evoforge.org>
2019-09-10 15:17:30 -0400

    #16 support for multiple handlers in reporter directive.

--- src/directive/SkillsReporterDirective.js
+++ src/directive/SkillsReporterDirective.js
@@ -2,34 +2,32 @@ import SkillsReporter from '@skills/skills-client-reporter';
 
 import debounce from 'lodash/debounce';
 
-const CONFIGURATION_OPTIONS = {
-  globalSuccessHandler: 'globalSuccessHandler',
-  globalErrorHandler: 'globalErrorHandler'
-};
 const SUCCESS_EVENT = 'skills-report-success';
 const FAILURE_EVENT = 'skills-report-error';
 
+const successHandlerCache = new Set();
+const errorHandlerCache = new Set();
 const eventCache = new WeakMap();
-const globalConfiguration = { };
 
-const hasGlobalSuccessHandlerConfigured = () => globalConfiguration[CONFIGURATION_OPTIONS.globalSuccessHandler];
-const hasGlobalErrorHandlerConfigured = () => globalConfiguration[CONFIGURATION_OPTIONS.globalSuccessHandler];
+const callSuccessHandlers = (event) => {
+  successHandlerCache.forEach(it => it(event));
+};
+
+const callErrorHandlers = (event) => {
+  errorHandlerCache.forEach(it => it(event));
+};
 
 const eventListener = (el, skillId) => {
   return debounce(() => {
     SkillsReporter.reportSkill(skillId)
       .then((result) => {
         const event = new CustomEvent(SUCCESS_EVENT, { detail: result });
-        if (hasGlobalSuccessHandlerConfigured()) {
-          globalConfiguration[CONFIGURATION_OPTIONS.globalSuccessHandler](event);
-        }
+        callSuccessHandlers(event);
         el.dispatchEvent(event);
       })
       .catch((error) => {
         const event = new CustomEvent(FAILURE_EVENT, { detail: error });
-        if (hasGlobalErrorHandlerConfigured()) {
-          globalConfiguration[CONFIGURATION_OPTIONS.globalErrorHandler](event);
-        }
+        callErrorHandlers(event);
         el.dispatchEvent(event);
       });
   }, 500)
@@ -37,14 +35,11 @@ const eventListener = (el, skillId) => {
 
 export default {
   name: 'skills',
-  configure(options) {
-    const validOptions = Object.values(CONFIGURATION_OPTIONS);
-    Object.keys(options).forEach((option) => {
-      if (!validOptions.includes(option)) {
-        throw new Error(`Invalid option passed to the SkillsReporterDirective option=[${option}]\nValid options are [${validOptions.join(', ')}]`);
-      }
-      globalConfiguration[option] = options[option];
-    });
+  addSuccessHandler(handler) {
+    successHandlerCache.add(handler);
+  },
+  addErrorHandler(handler) {
+    errorHandlerCache.add(handler);
   },
   inserted(el, binding) {
     const eventContext = {
