89e1e1fd0cbd2c23c6bcf6049f3ae1467879ead5
Matthew Kirkley <mtkirkl@evoforge.org>
2019-09-24 11:44:31 -0400

    #19 SkillsDisplay.vue is now using native iframe code

--- src/component/SkillsDisplay.vue
+++ src/component/SkillsDisplay.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="iframeContainer" />
+  <div ref="clientDisplayContainer" />
 </template>
 
 <script>
@@ -9,8 +9,7 @@
   import axios from 'axios';
 
   import SkillsConfiguration from '@skills/skills-client-configuration';
-
-  Vue.use(VueScrollTo);
+  import SkillsClientNative from '@skills/skills-client-native';
 
   export default {
     props: {
@@ -50,81 +49,22 @@
         default: null,
       },
     },
-    data() {
-      return {
-        authenticationPromise: null, // Vuex would be more appropriate
-      };
-    },
-    computed: {
-      configuration() {
-        const serviceUrl = this.options.serviceUrl ? this.options.serviceUrl : SkillsConfiguration.getServiceUrl();
-        const authenticator = this.options.authenticator ? this.options.authenticator : SkillsConfiguration.getAuthenticator();
-        const projectId = this.options.projectId ? this.options.projectId : SkillsConfiguration.getProjectId();
-        return { serviceUrl, authenticator, projectId };
-      }
-    },
     watch: {
       version(newValue) {
-        this.childFrame.call('updateVersion', newValue);
+        this.clientDisplay.version = newValue;
       },
     },
     mounted() {
-      const configuration = this.configuration;
-      const handshake = new Postmate({
-        container: this.$refs.iframeContainer,
-        url: `${configuration.serviceUrl}/static/clientPortal/index.html`,
-        classListArray: ['client-display-iframe'],
-        model: {
-          serviceUrl: configuration.serviceUrl,
-          projectId: configuration.projectId,
-          version: this.version,
-          userId: configuration.authenticator === 'pki' ? this.userId: null,
-          theme: this.theme,
-          minHeight: '960px',
-        },
+      this.clientDisplay = new SkillsClientNative({
+        options: this.options,
+        version: this.version,
+        theme: this.theme,
+        userId: this.userId,
       });
-
-      this.$refs.iframeContainer.height = 0;
-      this.$refs.iframeContainer.style.height = '0px';
-      // Make height changes smoother behind the scenes when the iframe is building.
-      // this.$refs.iframeContainer.style.backgroundColor = this.theme.backgroundColor ? this.theme.backgroundColor : 'white';
-
-      handshake.then((child) => {
-        this.childFrame = child;
-        child.on('height-changed', (data) => {
-          const adjustedHeight = Math.max(data, 960);
-          this.$refs.iframeContainer.height = adjustedHeight;
-          this.$refs.iframeContainer.style.height = `${adjustedHeight}px`;
-        });
-        child.on('route-changed', () => {
-          if (!this.options.disableAutoScroll) {
-            let scrollToElement = this.$refs.iframeContainer;
-            if (this.options.autoScrollStrategy === 'top-of-page') {
-              scrollToElement = document.body;
-            }
-            VueScrollTo.scrollTo(scrollToElement, 1000, {
-              y: true,
-              x: false,
-            });
-          }
-        });
-        child.on('needs-authentication', () => {
-          if (!this.authenticationPromise && this.configuration.authenticator !== 'pki') {
-            this.authenticationPromise = axios.get(this.configuration.authenticator)
-              .then((result) => {
-                child.call('updateAuthenticationToken', result.data.access_token);
-              })
-              .finally(() => {
-                this.authenticationPromise = null;
-              });
-          } else if (this.configuration.authenticator === 'pki') {
-            child.call('updateAuthenticationToken', 'pki');
-          }
-        });
-      });
+      this.clientDisplay.attachTo(this.$refs.clientDisplayContainer);
     },
     beforeDestroy() {
-      this.childFrame.destroy();
+      this.clientDisplay.destroy();
     },
   };
 </script>
