5c3f8010fc8fc88ce124840fc374e574e05d79ce
Matthew Kirkley <mtkirkl@evoforge.org>
2019-10-01 12:52:29 -0400

    #19 version bump

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import SkillsConfiguration from '@skills/skills-client-configuration';
-import SkillsReporter from ',/SkillsReporter.js';
-import SkillsDisplayJS from './SkillsDisplayJS.js';
+import SkillsDisplayJS from './SkillsDisplayJS';
+import { SkillsReporter, SUCCESS_EVENT, FAILURE_EVENT } from './SkillsReporter';
 
 export {
   SkillsConfiguration,
   SkillsReporter,
+  SUCCESS_EVENT,
+  FAILURE_EVENT,
   SkillsDisplayJS,
 };
