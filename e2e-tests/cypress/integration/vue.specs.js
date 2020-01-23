a4fbca2c226a68074936e5146332e956238bc98b
Ryan Mayo <rmmayo@evoforge.org>
2020-01-23 13:58:38 -0500

    #48 - debugging

--- e2e-tests/cypress/integration/vue.specs.js
+++ e2e-tests/cypress/integration/vue.specs.js
@@ -104,47 +104,47 @@ context('Vue Tests', () => {
         cy.contains('Level 0')
     })
 
-    // it('v-skill directive on click', () => {
-    //     cy.createDefaultProject(1, 2, 50, 2)
+    it('v-skill directive on click', () => {
+        cy.createDefaultProject(1, 2, 50, 2)
 
-    //     cy.server().route('POST', '/api/projects/proj1/skills/IronMan').as('postSkill')
+        cy.server().route('POST', '/api/projects/proj1/skills/IronMan').as('postSkill')
 
-    //     cy.visit('/vuejs#/')
+        cy.visit('/vuejs#/')
 
-    //     Cypress.Commands.add("clickOnDirectiveBtn", (skillApplied = true) => {
-    //         cy.get('#SkillsDirectiveClickEvent button').click()
-    //         cy.wait('@postSkill')
-    //         cy.get('@postSkill').then((xhr) => {
-    //             expect(xhr.status).to.eq(200)
-    //             expect(xhr.responseBody).to.have.property('skillApplied').to.eq(skillApplied)
-    //         });
-    //     })
+        Cypress.Commands.add("clickOnDirectiveBtn", (skillApplied = true) => {
+            cy.get('#SkillsDirectiveClickEvent button').click()
+            cy.wait('@postSkill')
+            cy.get('@postSkill').then((xhr) => {
+                expect(xhr.status).to.eq(200)
+                expect(xhr.responseBody).to.have.property('skillApplied').to.eq(skillApplied)
+            });
+        })
 
-    //     cy.clickOnDirectiveBtn()
-    //     cy.clickOnDirectiveBtn()
-    //     cy.clickOnDirectiveBtn(false)
-    // })
+        cy.clickOnDirectiveBtn()
+        cy.clickOnDirectiveBtn()
+        cy.clickOnDirectiveBtn(false)
+    })
 
 
-    // it('v-skill directive on input', () => {
-    //     cy.createDefaultProject(1, 2, 50, 2)
-    //     cy.server().route('POST', '/api/projects/proj1/skills/Thor').as('postSkill')
+    it('v-skill directive on input', () => {
+        cy.createDefaultProject(1, 2, 50, 2)
+        cy.server().route('POST', '/api/projects/proj1/skills/Thor').as('postSkill')
 
-    //     cy.visit('/vuejs#/')
+        cy.visit('/vuejs#/')
 
-    //     Cypress.Commands.add("typeToInput", (skillApplied = true) => {
-    //         cy.get('#SkillsDirectiveInputEvent input').type('h')
-    //         cy.wait('@postSkill')
-    //         cy.get('@postSkill').then((xhr) => {
-    //             expect(xhr.status).to.eq(200)
-    //             expect(xhr.responseBody).to.have.property('skillApplied').to.eq(skillApplied)
-    //         });
-    //     })
+        Cypress.Commands.add("typeToInput", (skillApplied = true) => {
+            cy.get('#SkillsDirectiveInputEvent input').type('h')
+            cy.wait('@postSkill')
+            cy.get('@postSkill').then((xhr) => {
+                expect(xhr.status).to.eq(200)
+                expect(xhr.responseBody).to.have.property('skillApplied').to.eq(skillApplied)
+            });
+        })
 
-    //     cy.typeToInput()
-    //     cy.typeToInput()
-    //     cy.typeToInput(false)
-    // })
+        cy.typeToInput()
+        cy.typeToInput()
+        cy.typeToInput(false)
+    })
 
     // it('v-skill directive on click with error', () => {
     //     cy.createDefaultTinyProject()
@@ -174,85 +174,85 @@ context('Vue Tests', () => {
     //     });
     // })
 
-    // it('skill display', () => {
-    //     cy.createDefaultTinyProject()
-    //     cy.server().route('/api/users/user1/token').as('getToken')
-    //     cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
-    //     cy.visit('/vuejs#/showSkills')
-    //     cy.wait('@getToken')
+    it('skill display', () => {
+        cy.createDefaultTinyProject()
+        cy.server().route('/api/users/user1/token').as('getToken')
+        cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
+        cy.visit('/vuejs#/showSkills')
+        cy.wait('@getToken')
 
-    //     cy.iframe((body) => {
-    //         cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wait('@getToken')
 
-    //         cy.wrap(body).contains('My Level')
-    //         cy.wrap(body).contains('50 Points earned Today')
-    //         cy.wrap(body).contains('Subject 0')
+            cy.wrap(body).contains('My Level')
+            cy.wrap(body).contains('50 Points earned Today')
+            cy.wrap(body).contains('Subject 0')
 
-    //         // verify that there is no background set
-    //         // cypress always validates against rgb
-    //         cy.wrap(body).find('.skills-page-title-text-color')
-    //             .should('have.css', 'background-color').and('equal', 'rgb(255, 255, 255)');
-    //     });
-    // })
+            // verify that there is no background set
+            // cypress always validates against rgb
+            cy.wrap(body).find('.skills-page-title-text-color')
+                .should('have.css', 'background-color').and('equal', 'rgb(255, 255, 255)');
+        });
+    })
 
-    // it('skill display - summary only', () => {
-    //     cy.createDefaultTinyProject()
-    //     cy.server().route('/api/users/user1/token').as('getToken')
-    //     cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
-    //     cy.visit('/vuejs#/showSkills?isSummaryOnly=true')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wait('@getToken')
-    //         cy.wrap(body).contains('My Level')
-    //         cy.wrap(body).contains('50 Points earned Today')
-    //         cy.wrap(body).contains('Subject 0').should('not.exist')
+    it('skill display - summary only', () => {
+        cy.createDefaultTinyProject()
+        cy.server().route('/api/users/user1/token').as('getToken')
+        cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
+        cy.visit('/vuejs#/showSkills?isSummaryOnly=true')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wait('@getToken')
+            cy.wrap(body).contains('My Level')
+            cy.wrap(body).contains('50 Points earned Today')
+            cy.wrap(body).contains('Subject 0').should('not.exist')
 
-    //         // verify that there is no background set
-    //         // cypress always validates against rgb
-    //         cy.wrap(body).find('.skills-page-title-text-color')
-    //             .should('have.css', 'background-color').and('equal', 'rgb(255, 255, 255)');
-    //     })
-    // })
-
-
-    // it('skill display - theme', () => {
-    //     cy.createDefaultTinyProject()
-    //     cy.server().route('/api/users/user1/token').as('getToken')
-    //     cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
-    //     cy.visit('/vuejs#/showSkills?themeName=Dark Blue')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wait('@getToken')
-    //         cy.wrap(body).contains('My Level')
-    //         cy.wrap(body).contains('50 Points earned Today')
-    //         cy.wrap(body).contains('Subject 0')
-
-    //         // verify dark blue background of hex #152E4d
-    //         // cypress always validates against rgb
-    //         cy.wrap(body).find('.skills-page-title-text-color')
-    //             .should('have.css', 'background-color').and('equal', 'rgb(21, 46, 77)');
-    //     })
-    // })
+            // verify that there is no background set
+            // cypress always validates against rgb
+            cy.wrap(body).find('.skills-page-title-text-color')
+                .should('have.css', 'background-color').and('equal', 'rgb(255, 255, 255)');
+        })
+    })
 
 
-    // it('skill display - summary only - theme', () => {
-    //     cy.createDefaultTinyProject()
-    //     cy.server().route('/api/users/user1/token').as('getToken')
-    //     cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
-    //     cy.visit('/vuejs#/showSkills?themeName=Dark Blue&isSummaryOnly=true')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wait('@getToken')
-    //         cy.wrap(body).contains('My Level')
-    //         cy.wrap(body).contains('50 Points earned Today')
-    //         cy.wrap(body).contains('Subject 0').should('not.exist')
+    it('skill display - theme', () => {
+        cy.createDefaultTinyProject()
+        cy.server().route('/api/users/user1/token').as('getToken')
+        cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
+        cy.visit('/vuejs#/showSkills?themeName=Dark Blue')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wait('@getToken')
+            cy.wrap(body).contains('My Level')
+            cy.wrap(body).contains('50 Points earned Today')
+            cy.wrap(body).contains('Subject 0')
 
-    //         // verify dark blue background of hex #152E4d
-    //         // cypress always validates against rgb
-    //         cy.wrap(body).find('.skills-page-title-text-color')
-    //             .should('have.css', 'background-color').and('equal', 'rgb(21, 46, 77)');
-    //     })
-    // })
+            // verify dark blue background of hex #152E4d
+            // cypress always validates against rgb
+            cy.wrap(body).find('.skills-page-title-text-color')
+                .should('have.css', 'background-color').and('equal', 'rgb(21, 46, 77)');
+        })
+    })
+
+
+    it('skill display - summary only - theme', () => {
+        cy.createDefaultTinyProject()
+        cy.server().route('/api/users/user1/token').as('getToken')
+        cy.backendPost('/api/projects/proj1/skills/Thor', {userId: 'user1', timestamp: Date.now()})
+        cy.visit('/vuejs#/showSkills?themeName=Dark Blue&isSummaryOnly=true')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wait('@getToken')
+            cy.wrap(body).contains('My Level')
+            cy.wrap(body).contains('50 Points earned Today')
+            cy.wrap(body).contains('Subject 0').should('not.exist')
+
+            // verify dark blue background of hex #152E4d
+            // cypress always validates against rgb
+            cy.wrap(body).find('.skills-page-title-text-color')
+                .should('have.css', 'background-color').and('equal', 'rgb(21, 46, 77)');
+        })
+    })
 
     // it('client display should display an error if skills service is down', () => {
     //     cy.server().route({
@@ -268,33 +268,33 @@ context('Vue Tests', () => {
     // });
 
 
-    // it('only display skills up-to the provided version', () => {
-    //     cy.createDefaultTinyProject()
-    //     cy.server().route('/api/users/user1/token').as('getToken')
-    //     cy.backendAddSkill('skillv1', 1)
-    //     cy.backendAddSkill('skillv2', 2)
-    //     cy.visit('/vuejs#/showSkills')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wait('@getToken')
-    //         cy.wrap(body).contains('Earn up to 200 points')
-    //     })
+    it('only display skills up-to the provided version', () => {
+        cy.createDefaultTinyProject()
+        cy.server().route('/api/users/user1/token').as('getToken')
+        cy.backendAddSkill('skillv1', 1)
+        cy.backendAddSkill('skillv2', 2)
+        cy.visit('/vuejs#/showSkills')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wait('@getToken')
+            cy.wrap(body).contains('Earn up to 200 points')
+        })
 
-    //     cy.visit('/vuejs#/')
-    //     cy.visit('/vuejs#/showSkills?skillsVersion=1')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wrap(body).contains('Earn up to 150 points')
-    //     })
+        cy.visit('/vuejs#/')
+        cy.visit('/vuejs#/showSkills?skillsVersion=1')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wrap(body).contains('Earn up to 150 points')
+        })
 
-    //     cy.visit('/vuejs#/')
-    //     cy.visit('/vuejs#/showSkills?skillsVersion=0')
-    //     cy.wait('@getToken')
-    //     cy.iframe((body) => {
-    //         cy.wrap(body).contains('Earn up to 100 points')
-    //     })
+        cy.visit('/vuejs#/')
+        cy.visit('/vuejs#/showSkills?skillsVersion=0')
+        cy.wait('@getToken')
+        cy.iframe((body) => {
+            cy.wrap(body).contains('Earn up to 100 points')
+        })
 
-    // });
+    });
 
 
 })
