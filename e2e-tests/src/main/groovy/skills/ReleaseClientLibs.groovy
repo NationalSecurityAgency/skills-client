d13637c520dd9ac547b49bda92e0894034262763
EVOFORGE\dimay <dimay@evoforge.org>
2020-02-14 08:50:58 -0500

    #12: release script

--- e2e-tests/src/main/groovy/skills/ReleaseClientLibs.groovy
+++ e2e-tests/src/main/groovy/skills/ReleaseClientLibs.groovy
@@ -11,14 +11,17 @@ class ReleaseClientLibs {
         String releaseTypeStr = releaseTypeParam.split("-releaseType=")[1]
         assert ReleaseType.values().find({ it.name() == releaseTypeStr }), "Must provide releaseType=${ReleaseType.values().collect({ it.name() }).join("|")}"
         ReleaseType releaseType = ReleaseType.valueOf(releaseTypeStr)
+        Boolean notDryRun = args.find({ it.equalsIgnoreCase("-notDryRun") })
 
-        new ReleaseClientLibs(releaseType: releaseType).release()
+        new ReleaseClientLibs(releaseType: releaseType, dryRun: !notDryRun).release()
     }
 
     enum ReleaseType { patch, minor, major, prepatch, preminor, premajor}
     ReleaseType releaseType
     File workDir = new File("./e2e-tests/target/${ReleaseClientLibs.simpleName}/")
     TitlePrinter titlePrinter = new TitlePrinter()
+    boolean dryRun = true
+
     void release(){
         titlePrinter.printTitle("Release Client Libs: Release type=[${releaseType}]")
         DirHelper.createEmptyDirClearIfExist(workDir)
@@ -31,7 +34,6 @@ class ReleaseClientLibs {
             log.info("${it.from.name} (${it.from.version}) => ${it.to.name} (${it.to.version})")
         }
 
-        boolean dryRun = true
         for (NpmProj proj in npmProjects.allProj.findAll({ it.doOthersLinkToMe })) {
             titlePrinter.printTitle("Release for ${proj.name}")
             if (proj.hasUnreleasedChanges()) {
@@ -41,14 +43,21 @@ class ReleaseClientLibs {
                 proj.exec("npm run build", dryRun)
                 proj.exec("release-it --ci ${releaseType}", dryRun)
 
-                List<NpmProjRel> updateVersion = rels.findAll({ it.to.name == proj.name })
-                for (NpmProjRel updateRel in updateVersion) {
-                    log.info("  Update version for [${updateRel.from.name}]: [${updateRel.from.getDepVersion("@skills/${updateRel.to.name}")}] -> [${updateRel.to.version}]")
-                    updateRel.from.exec("npm install --save @skills/${updateRel.to.name}@${updateRel.to.version}", dryRun)
-                    updateRel.from.exec("git push", dryRun)
-                }
             } else {
                 log.info("${proj.name} has no changes. Release is not needed!")
+            }
+
+            List<NpmProjRel> updateVersion = rels.findAll({ it.to.name == proj.name })
+            for (NpmProjRel updateRel in updateVersion) {
+                String fromVersion = updateRel.from.getDepVersion("@skills/${updateRel.to.name}");
+                if (!fromVersion.equalsIgnoreCase(updateRel.to.version)) {
+                    log.info("  Update version for [${updateRel.from.name}]: ${updateRel.to.name} [${fromVersion}] -> [${updateRel.to.version}]")
+                    updateRel.from.exec("npm install --save-exact @skills/${updateRel.to.name}@${updateRel.to.version}", dryRun)
+                    updateRel.from.exec("git commit -a -m incrementing", dryRun)
+                    updateRel.from.exec("git push", dryRun)
+                } else {
+                    log.info("   [${updateRel.from.name}]: is already up-to-date with version ${updateRel.to.name}:${updateRel.to.version}")
+                }
             }
         }
 
