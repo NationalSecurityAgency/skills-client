8a4ed15ea303bf56f88459c1223dc124fc1641aa
EVOFORGE\dimay <dimay@evoforge.org>
2020-03-09 10:13:08 -0400

    #1: CI - WIP

--- skills-client-integration/ci/setupNpmLinks.sh
+++ skills-client-integration/ci/setupNpmLinks.sh
@@ -13,18 +13,7 @@
 # limitations under the License.
 #!/usr/bin/env bash
 
-apt-get install -y gawk
-
-majorVersion=1.1
-latestSnapVersion=`curl -s http://$NEXUS_SERVER/repository/maven-snapshots/skills/backend/maven-metadata.xml | grep "<version>${majorVersion}" | gawk -F "version>" '{print $2}' | gawk -F "<" '{print $1}' | sort | tac  | head -n 1`
-
-if [ -z "$latestSnapVersion" ]
-then
-      echo "Failed to locate SNAPSHOT version that start with ${1}"
-      exit -1
-fi
-
-mkdir -p ./skills-service/
-cd ./skills-service/
-mvn --batch-mode dependency:get -Dartifact=skills:backend:${latestSnapVersion}:jar -Dtransitive=false -Ddest=backend-toTest.jar
-cd ../
+cd skills-client-integration/skills-int-e2e-test
+mvn --batch-mode clean package
+java -cp target/skills-example-e2e-*.jar -Dloader.main=skills.SetupNpmLinks org.springframework.boot.loader.PropertiesLauncher
+cd ../../
