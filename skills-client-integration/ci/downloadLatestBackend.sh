efee41c9217e9de6facc932f7068ed8ac0d48659
EVOFORGE\dimay <dimay@evoforge.org>
2020-03-10 15:37:44 -0400

    #1: CI - WIP

--- skills-client-integration/ci/downloadLatestBackend.sh
+++ skills-client-integration/ci/downloadLatestBackend.sh
@@ -25,8 +25,11 @@ myGitBranch=`git ls-remote --heads origin | grep $(git rev-parse HEAD) | gawk -F
 echo "My Git Branch: [${myGitBranch}]"
 
 latestSnapVersion=''
-if "${myGitBranch}" | sed 's/\.X$//' || "${myGitBranch}" == "master"
+endsWithX=`echo ${myGitBranch} | grep '.X$'`
+echo "Branch ends with .X: ${endsWithX}"
+if [ $endsWithX -eq 0 ] || [ "${myGitBranch}" == "master" ]
 then
+    echo "Will download artifact from Nexus"
     majorVersion=''
     if [ "${myGitBranch}" != "master" ]
     then
@@ -36,11 +39,12 @@ then
     echo "Version to look for is [${majorVersion}], if blank then latest version will be used!"
     latestSnapVersion=`curl -s http://$NEXUS_SERVER/repository/maven-snapshots/skills/backend/maven-metadata.xml | grep "<version>${majorVersion}" | gawk -F "version>" '{print $2}' | gawk -F "<" '{print $1}' | sort | tac  | head -n 1`
     echo "Latest snapshot version: [${latestSnapVersion}]"
-fi
-
-if [ -z "$latestSnapVersion" ]
-then
-    echo "Failed to locate SNAPSHOT on nexus. Let's checkout skill-service code and build it ourselves."
+    mkdir -p ./skills-service/
+    cd ./skills-service/
+    echo "Downloading: ${latestSnapVersion}"
+    mvn --batch-mode dependency:get -Dartifact=skills:backend:${latestSnapVersion}:jar -Dtransitive=false -Ddest=backend-toTest.jar
+else
+    echo "Will checkout skill-service code and build it."
     git clone https://${DEPLOY_TOKEN_SKILLS_SERVICE}:${DEPLOY_TOKEN_SKILLS_SERVICE_PASS}@gitlab.evoforge.org/skills/skills-service.git
     cd ./skills-service/
     git branch -a | grep dimay
@@ -56,11 +60,6 @@ then
     echo "git status:"
     git status
     mvn --batch-mode package -DskipTests
-else
-    mkdir -p ./skills-service/
-    cd ./skills-service/
-    echo "Downloading: ${latestSnapVersion}"
-    mvn --batch-mode dependency:get -Dartifact=skills:backend:${latestSnapVersion}:jar -Dtransitive=false -Ddest=backend-toTest.jar
 fi
 
 cd ../
