ed54d749c01d664a5baa197a79f901332848d5fa
EVOFORGE\dimay <dimay@evoforge.org>
2020-08-28 15:32:06 -0400

    #34: improve errors handling; enhance errors' messages

--- skills-client-js/test/config/SkillsConfiguration.test.js
+++ skills-client-js/test/config/SkillsConfiguration.test.js
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 import SkillsConfiguration from '../../src/config/SkillsConfiguration.js';
+import {SkillsReporter} from "../../src";
 
 describe('SkillsConfiguration', () => {
   describe('initialization', () => {
@@ -21,9 +22,15 @@ describe('SkillsConfiguration', () => {
       expect(SkillsConfiguration).not.toBeNull();
     });
 
+    it('throws an error SkillsConfiguration.configure() was never called', () => {
+      expect(() => {
+        SkillsConfiguration.validate();
+      }).toThrow();
+    });
+
     it('setAuthToken updates the authToken', () => {
       const mockAuthToken = Math.random() + 1;
-      SkillsConfiguration.configure({ authToken: mockAuthToken });
+      SkillsConfiguration.configure({ authToken: mockAuthToken, projectId: 'projectId', serviceUrl: 'http://somewhere' });
 
       expect(SkillsConfiguration.getAuthToken()).toBe(mockAuthToken);
 
@@ -39,7 +46,7 @@ describe('SkillsConfiguration', () => {
           const mockAuthenticator = Math.random();
 
           SkillsConfiguration.configure({
-            authenticator: mockAuthenticator,
+            authenticator: mockAuthenticator, projectId: 'proj', serviceUrl: 'http://somewhere',
           });
 
           expect(SkillsConfiguration.isPKIMode()).toBeFalsy();
@@ -49,7 +56,7 @@ describe('SkillsConfiguration', () => {
           const mockAuthenticator = 'pki';
 
           SkillsConfiguration.configure({
-            authenticator: mockAuthenticator,
+            authenticator: mockAuthenticator, projectId: 'proj', serviceUrl: 'http://somewhere',
           });
 
           expect(SkillsConfiguration.isPKIMode()).toBeTruthy();
@@ -87,47 +94,67 @@ describe('SkillsConfiguration', () => {
           done();
         });
 
-        SkillsConfiguration.configure({ });
+        SkillsConfiguration.configure({ authenticator: 'pki', projectId: 'proj', serviceUrl: 'http://somewhere' });
       });
     });
 
     describe('validation', () => {
-      const verifyThrows = (configObject) => {
-        SkillsConfiguration.configure(configObject);
-
-        expect(() => {
-          SkillsConfiguration.validate();
-        }).toThrow();
-      };
-
       it('throws an error if projectId is not set', () => {
-        const mockServiceUrl = Math.random();
-        const mockAuthenticator = Math.random();
+        expect(() => {
+          SkillsConfiguration.configure({
+            serviceUrl: 'http://somewhere',
+            authenticator: 'pki',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for projectId=[undefined]');
+      });
 
-        verifyThrows({
-          serviceUrl: mockServiceUrl,
-          authenticator: mockAuthenticator,
-        });
+      it('throws an error if projectId is set to null', () => {
+        expect(() => {
+          SkillsConfiguration.configure({
+            projectId: null,
+            serviceUrl: 'http://somewhere',
+            authenticator: 'pki',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for projectId=[null]');
+      });
+
+      it('throws an error if serviceUrl is set to null', () => {
+        expect(() => {
+          SkillsConfiguration.configure({
+            projectId: 'proj',
+            serviceUrl: null,
+            authenticator: 'pki',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for serviceUrl=[null]');
       });
 
       it('throws an error if serviceUrl is not set', () => {
-        const mockProjectId = Math.random();
-        const mockAuthenticator = Math.random();
+        expect(() => {
+          SkillsConfiguration.configure({
+            projectId: 'proj',
+            authenticator: 'pki',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for serviceUrl=[undefined]');
+      });
 
-        verifyThrows({
-          projectId: mockProjectId,
-          authenticator: mockAuthenticator,
-        });
+
+      it('throws an error if authenticator is set to null', () => {
+        expect(() => {
+          SkillsConfiguration.configure({
+            projectId: 'proj',
+            serviceUrl: 'http://some',
+            authenticator: 'null',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for authenticator=[null]');
       });
 
       it('throws an error if authenticator is not set', () => {
-        const mockProjectId = Math.random();
-        const mockServiceUrl = Math.random();
-
-        verifyThrows({
-          projectId: mockProjectId,
-          serviceUrl: mockServiceUrl,
-        });
+        expect(() => {
+          SkillsConfiguration.configure({
+            projectId: 'proj',
+            serviceUrl: 'http://some',
+          });
+        }).toThrow('SkillTree: SkillsConfiguration.configure received invalid parameter for authenticator=[undefined]');
       });
 
       it('does not throw an error if everything is set', () => {
@@ -162,7 +189,12 @@ describe('SkillsConfiguration', () => {
 
       it('clears the authToken', () => {
         const mockAuthToken = Math.random();
-        SkillsConfiguration.configure({ authToken: mockAuthToken });
+        SkillsConfiguration.configure({
+          authToken: mockAuthToken,
+          projectId: 'proj',
+          serviceUrl: 'http://some',
+          authenticator: 'http://auth',
+        });
 
         expect(SkillsConfiguration.getAuthToken()).toBe(mockAuthToken);
 
