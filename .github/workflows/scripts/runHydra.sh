e6c792fc9ade1d6fb9e59f21a279c999694deca1
rmmayo <rmmayo@evoforge.org>
2020-10-12 11:59:22 -0400

    #57 - add hydra support

---
+++ .github/workflows/scripts/runHydra.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DB=${DB:-postgres}
+TRACING=${TRACING:-false}
+PROMETHEUS=${PROMETHEUS:-false}
+cd hyrda
+DC="docker-compose -f quickstart.yml"
+if [[ $DB == "mysql" ]]; then
+    DC+=" -f quickstart-mysql.yml"
+fi
+if [[ $DB == "postgres" ]]; then
+    DC+=" -f quickstart-postgres.yml"
+fi
+if [[ $TRACING == true ]]; then
+    DC+=" -f quickstart-tracing.yml"
+fi
+if [[ $PROMETHEUS == true ]]; then
+    DC+=" -f quickstart-prometheus.yml"
+fi
+DC+=" up --build"
+
+$DC
+
