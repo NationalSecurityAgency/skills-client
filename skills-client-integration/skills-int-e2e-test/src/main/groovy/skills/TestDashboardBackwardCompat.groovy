258b0625d42ccf48c6f27bc8cd5f5612dcbb3ca4
rmmayo <rmmayo@evoforge.org>
2023-03-10 15:21:25 -0500

    #205 - attempt using alternative JAVA_HOME per version if configured

--- skills-client-integration/skills-int-e2e-test/src/main/groovy/skills/TestDashboardBackwardCompat.groovy
+++ skills-client-integration/skills-int-e2e-test/src/main/groovy/skills/TestDashboardBackwardCompat.groovy
@@ -19,14 +19,33 @@ import groovy.cli.picocli.CliBuilder
 import groovy.json.JsonOutput
 import groovy.util.logging.Slf4j
 import org.apache.commons.io.FileUtils
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.core.io.Resource
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.core.io.support.ResourcePatternResolver
 
 @Slf4j
-class TestDashboardBackwardCompat {
+@SpringBootApplication
+class TestDashboardBackwardCompat implements CommandLineRunner {
+
+    File workDir = new File("./target/skills-client")
+    TitlePrinter titlePrinter = new TitlePrinter()
+    List<String> versionsToExclude = ["2.0.0"]
+    boolean recordInDashboard = false
+    String tag
+
+    @Autowired
+    Config config
 
     static void main(String[] args) {
+        SpringApplication.run(TestDashboardBackwardCompat.class, args)
+    }
+
+    @Override
+    void run(String... args) {
         def cli = new CliBuilder()
         cli.record(type: boolean, 'record tests in cypress dashboard')
         cli.tag(type: String, 'tag to give cypress test')
@@ -36,14 +55,12 @@ class TestDashboardBackwardCompat {
         if (options.tag && options.tag instanceof String) {
             tag = options.tag
         }
-        new TestDashboardBackwardCompat(recordInDashboard: shouldRecord, tag: tag).testNew()
-    }
+//        new TestDashboardBackwardCompat(recordInDashboard: shouldRecord, tag: tag).testNew()
 
-    File workDir = new File("./target/skills-client")
-    TitlePrinter titlePrinter = new TitlePrinter()
-    List<String> versionsToExclude = ["2.0.0"]
-    boolean recordInDashboard = false
-    String tag
+        this.recordInDashboard = shouldRecord
+        this.tag = tag
+        this.testNew()
+    }
 
     void checkoutWorkingCopy() {
         if (workDir.exists()) {
@@ -99,7 +116,7 @@ class TestDashboardBackwardCompat {
 
         String tagsStrs = new ProcessRunner(loc: workDir).run("git tag").sout
         assert tagsStrs
-        List<String> versionsToTest = tagsStrs.split("\n").toList();
+        List<String> versionsToTest = tagsStrs.split("\n").toList()
         versionsToTest.removeAll(versionsToExclude)
         log.info("Versions to test:\n  ${versionsToTest.join("\n  ")}")
 
@@ -123,7 +140,13 @@ class TestDashboardBackwardCompat {
             }
 
             titlePrinter.printSubTitle("[${version}]: Building Client Examples App")
-            new ProcessRunner(loc: clientIntLoc).run("mvn --batch-mode clean package")
+            String javaHomeAlt = config?.versionProps?.find {it.version == version}?.javaHomeAlt
+            if (javaHomeAlt) {
+                log.info("Using alternative JAVA_HOME [${javaHomeAlt}]")
+                new ProcessRunner(loc: clientIntLoc).run("JAVA_HOME=${javaHomeAlt} && mvn --batch-mode clean package")
+            } else {
+                new ProcessRunner(loc: clientIntLoc).run("mvn --batch-mode clean package")
+            }
 
             titlePrinter.printSubTitle("[${version}]: Running cypress test with")
             String output = "Testing version [$version]"
