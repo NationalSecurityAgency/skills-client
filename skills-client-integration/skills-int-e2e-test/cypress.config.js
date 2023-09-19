439caeae2cb50b078912c27e7d20c283cf86c621
Don Walizer <12420708+dwalizer@users.noreply.github.com>
2023-09-19 15:01:12 -0400

    #214 Further cypress upgrades

---
+++ skills-client-integration/skills-int-e2e-test/cypress.config.js
@@ -0,0 +1,20 @@
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  chromeWebSecurity: false,
+  defaultCommandTimeout: 10000,
+  projectId: '3yas4t',
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://localhost:8090',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})
